634f490dc1b62a9ed2a7f653b6701d39c2073471
Carlos Alexandre Neri <carlosalexandre.neri@gmail.com>
2020-02-14 20:56:32 -0300

    gitignore

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,9 @@
+// import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    return res.json({ mensage: 'acerto mizeravi' });
+  }
+}
+
+export default new AppointmentController();
