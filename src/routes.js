8ec1107052b02c589a3c7a346ba516eb6aee6767
Carlos Alexandre Neri <carlosalexandre.neri@gmail.com>
2020-02-19 00:50:19 -0300

    Notificando novos agendamentos

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,8 +20,14 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.get('/providers', ProviderController.index);
+
 routes.put('/users', UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
